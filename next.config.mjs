d6d99554601ef1544a5fd5c511ab11316c2b6cbb
Stélvio Baltazar <rcmidia.dev@gmail.com>
2026-08-10 10:42:18 +0100

    define URL publica da aplicacao no deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@rcmedia-dev/kima-sdk'],
+  env: {
+    NEXT_PUBLIC_APP_URL:
+      process.env.NEXT_PUBLIC_APP_URL || 'https://kima-facturacao.vercel.app',
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
